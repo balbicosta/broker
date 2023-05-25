c639be7a5dc9aea4e15dd13928f2e15fcf46e433
balbi <balbicosta@gmail.com>
2023-05-25 18:39:41 -0300

    Desenvolvimento da função e busca

--- _appFuncCad.py
+++ _appFuncCad.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QMainWindow
 from cadastro import Ui_Cadastro
 import sqlite3
 
+
 class anunciar(QMainWindow):
     try:
         def __init__(self, *args, **kwargs):
@@ -14,13 +15,31 @@ class anunciar(QMainWindow):
             self.tela1.pushButton_4.clicked.connect(self._limpar1)
             _ini = self.tela1.lineEdit.setText("0001")
 
-        def _buscar(self):
-            print("Função BUSCAR em desenvolvimento")
+        @staticmethod
+        def _buscar():
+            ide = input(print("Informe a identificação do imóvel: "))
+            try:
 
-        def _excluir(self):
+                banco = sqlite3.connect('imovel.db')
+                cursor = banco.cursor()
+                check = cursor.execute("SELECT ident FROM imovel WHERE ident = '"+ide+"' ")
+
+                banco.commit()
+                banco.close()
+            except Exception as e:
+                print("Imóvel não encontrado", e)
+
+        @staticmethod
+        def _excluir():
             print("Função EXCLUIR em desenvolvimento")
+            banco = sqlite3.connect('imovel.db')
+            cursor = banco.cursor()
+            cursor.execute("DROP imovel1.db")
+            banco.commit()
+            banco.close()
 
-        def _limpar1(self):
+        @staticmethod
+        def _limpar1():
             print("Função LIMPAR em desenvolvimento")
 
         def _salvar(self):
@@ -50,15 +69,16 @@ class anunciar(QMainWindow):
             cond = self.tela1.plainTextEdit_2.toPlainText()
 
             try:
-                banco = sqlite3.connect('imovel1.db')
+                banco = sqlite3.connect('imovel.db')
                 cursor = banco.cursor()
-                cursor.execute("CREATE TABLE IF NOT EXISTS imovel (ident text, ender text, numer text, complem text, cep text"
-                               ", bairro text, descric text, negocia text, tipo text, estado text, status text, preco text, "
-                               "quarto text, banho text, suite text, salaestar text, salajanta text, piscina text, arserv text,"
-                               "arlazer text, garag text, observ text, condic text)")
-                cursor.execute("INSERT INTO imovel VALUES ('"+ide+"', '"+end+"', '"+num+"', '"+comp+"', '"+cep+"', '"+bar+"',"
-                                                                                                                          "'"+desc+"','"+neg+"','"+tip+"','"+est+"','"+sta+"','"+prec+"','"+quar+"','"+ban+"','"+suite+"',"
-                                                                                                                                                                                                                       "'"+saest+"','"+saljan+"','"+pisc+"','"+arser+"','"+arlaz+"','"+gar+"','"+obs+"','"+cond+"')")
+                cursor.execute("CREATE TABLE IF NOT EXISTS imovel (ident text, ender text, numer text, complem text, "
+                               "cep text, bairro text, descric text, negocia text, tipo text, estado text, status text,"
+                               "preco text, quarto text, banho text, suite text, salaestar text, salajanta text, "
+                               "piscina text, arserv text, arlazer text, garag text, observ text, condic text)")
+                cursor.execute("INSERT INTO imovel VALUES ('"+ide+"', '"+end+"', '"+num+"', '"+comp+"', '"+cep+"', "
+                                "'"+bar+"','"+desc+"','"+neg+"','"+tip+"','"+est+"','"+sta+"','"+prec+"','"+quar+"',"
+                                "'"+ban+"','"+suite+"','"+saest+"','"+saljan+"','"+pisc+"','"+arser+"','"+arlaz+"',"
+                                "'"+gar+"','"+obs+"','"+cond+"')")
 
                 banco.commit()
                 banco.close()
